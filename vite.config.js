c98c9c33a6bf0762368dd281450148f4c2f47cda
dima92 <dima-tkachenko@mail.ru>
2026-08-20 21:32:56 +0300

    vite fix base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: "/shelter/",
   server: {
     host: true,
     port: 3000,
